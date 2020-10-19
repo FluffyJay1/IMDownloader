9771c4f760db4d75b3df217101590c09b98e6310
FluffyJay1 <michaelzcyang@gmail.com>
2020-10-19 16:09:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ImDownloader
+
+Small python script to bypass login requirements to download galleries from IMHentai
+
+I wrote this out of spite for the people who coded IMHentai's download function, maybe also for the 2 other people in the world who would find a use for this
+
+## Usage
+
+`python imdownloader.py <url>`
+
+Takes in only 1 argument, which is the url of the gallery you want (like https://imhentai.com/gallery/######)
+
+Creates a folder in the same directory with the name of the gallery, and downloads the images inside.
---
+++ imdownloader.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import sys
+import re
+
+url = sys.argv[1]
+html = requests.get(url).content
+regex = re.compile('gallery/(\d*)')
+m = regex.search(url)
+gallery_id = int(m.group(1))
+regex = re.compile(r"download_gallery\(\\'([^\\]*)\\', \\'([^\\]*)\\', \\'([^\\]*)\\'\)")
+m = regex.search(str(html))
+e = m.group(1) #url pt 2
+b = m.group(2) #url pt 1
+a = m.group(3) #album name 
+
+#fuckin galleryinfo
+galleryinfo_url = "http://imhentai.com/downloads/" + b + "/" + e + ".js"
+galleryinfo_raw = requests.get(galleryinfo_url).content
+regex = re.compile(r'\[(.*)\]')
+m = regex.search(str(galleryinfo_raw))
+galleryinfo = m.group(1).split(",")
+
+if (gallery_id > 0 and gallery_id <= 274825): 
+	server = 'm1.imhentai.com' 
+if (gallery_id > 274825 and gallery_id <= 551579): 
+	server = 'm2.imhentai.com'
+if (gallery_id > 551579):
+	server = 'm4.imhentai.com'
+	
+if not os.path.exists(a):
+    os.makedirs(a)
+
+regex = re.compile('"[^"]*":"(.*)"')
+total_images = len(galleryinfo)
+print("downloading " + str(total_images) + " images...")
+for i in range(total_images):
+	m = regex.search(galleryinfo[i])
+	n = m.group(1).split('|')
+	o = n[0]
+	l = "http://"+ server +"/" + n[1] + "/" + n[2] + "/" + o
+	print("downloading " + str(i + 1) + "/" + str(total_images) + ": " + o)
+	r = requests.get(l)
+	open(a + "/" + o, 'wb').write(r.content)
